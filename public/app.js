6847f2c0e14630bbb0a381462ba9429329699f5c
Demian Ferreiro <epidemian@gmail.com>
2012-04-27 01:15:12 -0300

    Get rid of jQuery as a dependency :) Also document a bit the functions on app.js

--- public/app.js
+++ public/app.js
@@ -1,11 +1,41 @@
-function log(str) {
-    $('#log').css('visibility', 'visible').prepend($('<p/>').text(str));
+// Encode a string containing HTML so it can be rendered as text.
+function encodeHtml(str) {
+    return str.replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;');
 }
 
+// Log a message to the screen.
+function log(message) {
+    var log = document.getElementById('log');
+    if (!log) return;
+
+    log.style.visibility = 'visible';
+    log.innerHTML += '<p>' + encodeHtml(message) + '</p>'
+}
+
+// Simple XMLHttpRequest wrapper for making GET requests. Calls options.success
+// callback with response text when the request is successful or options.error
+// if it isn't
+function ajaxGet(url, options) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            var callback = options[xhr.status == 200 ? 'success' : 'error'];
+            callback && callback(xhr.responseText);
+        }
+    };
+    xhr.open('get', url);
+    xhr.send(null);
+}
+
+// Acquire a Marlin Broadband Registration transaction token and then redeems it
+// using the NetTV drmAgent plugin. If everything goes right calls the onSuccess
+// callback.
 function drmRegister(onSuccess) {
-    log('downloading register token');
-    $.ajax('/register', {
-        dataType:'text',
+    log('Downloading register token...');
+    ajaxGet('/register', {
         success:function (registerToken) {
             var drmAgent = document.getElementById('drmAgent');
             drmAgent.onDRMMessageResult = function (msgID, resultMsg, resultCode) {
@@ -22,7 +52,7 @@ function drmRegister(onSuccess) {
             log("Sending to drmagent: " + registerToken);
         },
         error:function () {
-            log('error loading register token');
+            log('Error loading register token');
         }
     });
 }
